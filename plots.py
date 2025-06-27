55541992d5d2d5923bd422fb546721daa8939bae
CarlosGual <carlos.gutierrezalva@uah.es>
2025-06-27 12:57:18 +0200

    ``Refactor chapter to improve clarity and introduce acronyms
    Updated `ros4vsn.tex` to refine language, enhance readability, and align with standard formatting. Introduced `\acrshort{vsn}` acronym for consistency, updated figure labels, and clarified references. These changes improve document structure and readability for the target audience.```

--- plots.py
+++ plots.py
@@ -1,35 +1,43 @@
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib.lines import Line2D
 
 data = pd.read_csv('5.3.data/metanav.csv')
-
-# Sort by global_step to ensure lines connect in order
 data = data.sort_values('global_step')
 
-plt.figure(figsize=(10, 6), dpi=150)
-
-# Collect all columns ending with 'inner_action_loss'
+# Select relevant columns
 loss_columns = [col for col in data.columns if col.endswith('losses/inner_action_loss')]
-all_values = data[loss_columns].values
+all_data = data[loss_columns]
 
-average_values = np.mean(all_values, axis=1)
-std_values = np.std(all_values, axis=1)
+# Compute mean and std across columns for each row
+average_values = all_data.mean(axis=1)
+std_values = all_data.std(axis=1)
 
-plt.plot(data['global_step'], average_values, label='Average Inner Action Loss', color='blue', linewidth=2, marker='o')
-plt.fill_between(data['global_step'], average_values - std_values, average_values + std_values, color='blue', alpha=0.3)
+# Smooth the data using a rolling window
+window_size = 15
+average_values = average_values.rolling(window=window_size, min_periods=1).mean()
+std_values = std_values.rolling(window=window_size, min_periods=1).mean()
 
-plt.xlabel('Tasks', fontsize=18)
-plt.ylabel('Inner Action Loss', fontsize=18)
-plt.title('Average Inner Action Loss vs. Tasks', fontsize=18)
+# Use the corresponding global_step values
+global_steps = data.loc[all_data.index, 'global_step']
 
-plt.gca().set_axisbelow(True)
-plt.grid(True, linestyle='--', color='grey', alpha=0.20, which='both', axis='both')
+plt.figure(figsize=(10, 6), dpi=150)
+plt.plot(global_steps, average_values, label='Average Action Loss') # , color='violet')
+plt.fill_between(global_steps, average_values - std_values, average_values + std_values, color='gray', alpha=0.3)
+
+plt.xlabel('Steps', fontsize=18)
+plt.ylabel('Action Loss', fontsize=18)
+# plt.title('Average Inner Action Loss ± Std vs. Tasks', fontsize=18)
+plt.grid(True, linestyle='--', color='grey', alpha=0.20)
+plt.xlim(0, 94000000)
+# Change the ticks fontsize
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+
+# Put the scale in millions
+plt.gca().xaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{int(x/1e6)}M'))
+
 plt.tight_layout()
-plt.savefig('plots/meta_nav_losses.png')
-
+plt.legend()
+plt.savefig('plots/metanav_losses_avg_std.png')
 plt.show()
-
-
